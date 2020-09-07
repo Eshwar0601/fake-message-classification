9c04a58d09b04dd826324be298dbfe0f86d745a6
Eshwar0601 <852eshwar@gmai.com>
2020-09-07 11:41:27 +0530

    message alert

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Fake Message classification</h2>
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Please input the text message below</h2>
         <FormInput />
       </header>
     </div>
